90a680696704d90f27d4d28936caebbcb26d1d61
giusmili <giusmili67@gmail.com>
2026-09-22 15:05:11 +0200

    py

--- test.py
+++ test.py
@@ -9,3 +9,12 @@ print("Nom:", nom)
 print(type(nom))
 
 print(f"{nom} habite à {ville} et a {age} ans.")
+
+# test de la fonction condition
+for i in range(5):
+    print("Itération", i)
+
+compteur = 0
+while compteur < 3:
+    print("Compteur :", compteur)
+    compteur += 1
