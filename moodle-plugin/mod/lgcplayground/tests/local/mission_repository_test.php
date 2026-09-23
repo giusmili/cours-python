b00e5f1cc31fa37e1d50584c7c037ecfffd58540
Kevin Oudelet <kevin.oudelet@gmail.com>
2026-09-23 20:18:42 +0200

    test: étendre le contrat du pack Python

--- moodle-plugin/mod/lgcplayground/tests/local/mission_repository_test.php
+++ moodle-plugin/mod/lgcplayground/tests/local/mission_repository_test.php
@@ -20,7 +20,13 @@ final class mission_repository_test extends advanced_testcase {
         $this->assertSame('python-basics-v1', $pack['id']);
         $this->assertSame('python', $pack['track']);
         $this->assertSame(
-            ['python-00-terminal', 'python-01-variables', 'python-02-types'],
+            [
+                'python-00-terminal',
+                'python-01-variables',
+                'python-02-types',
+                'python-03-conditions',
+                'python-04-for-loop',
+            ],
             mission_repository::ids_for_track($pack, 'python'),
         );
 
