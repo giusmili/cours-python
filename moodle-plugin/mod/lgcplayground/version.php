a928d9ac40e9730ff6e98f731e6ec5481148d5af
Kevin Oudelet <kevin.oudelet@gmail.com>
2026-09-23 18:40:16 +0200

    feat: préparer la persistance Moodle du Playground

--- moodle-plugin/mod/lgcplayground/version.php
+++ moodle-plugin/mod/lgcplayground/version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_lgcplayground';
-$plugin->version = 2026092301;
+$plugin->version = 2026092302;
 $plugin->requires = 2026042003.01;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.2.0-alpha';
+$plugin->release = '0.3.0-alpha';
